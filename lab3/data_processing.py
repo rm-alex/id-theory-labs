57bc4d87877c2623e8e56ae0ab6412794f2543d5
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2025-12-14 11:07:35 +0300

    com

--- lab3/data_processing.py
+++ lab3/data_processing.py
@@ -27,12 +27,14 @@ def heatmap(data):
 df=pd.read_csv('lab3/Archive2025/data.csv')
 df=process_data(df)
 
+df['floor_ratio']=df['floor']/df['floor_max']
+
 heatmap(df)
 
-to_drop = ['gas', 'hot_water', 'central_heating',
-           'ceil_height', 'extra_area', 'extra_area_count',
-           'floor', 'bath_count', 'other_area', 'floor_max',
-           'year', 'extra_area_type_name_loggia']
+to_drop = ['gas', 'hot_water', 'ceil_height', 
+           'extra_area_count', 'floor', 'bath_count', 
+           'other_area', 'floor_max', 'year', 
+           'extra_area_type_name_loggia']
 
 df = df.drop(columns=to_drop)
 
@@ -43,5 +45,6 @@ df.to_csv('lab3/processed_data.csv', index=False)
 
 df_test = pd.read_csv('lab3/Archive2025/test.csv')
 df_test = process_data(df_test)
+df_test['floor_ratio']=df_test['floor']/df_test['floor_max']
 df_test = df_test.drop(columns=to_drop)
 df_test.to_csv('lab3/processed_test_data.csv', index=False)
